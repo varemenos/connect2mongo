a10a700741d631208aff093a1d6b172a70119eae
Adonis K <aklp08@gmail.com>
2014-10-25 14:24:01 +0300

    moved queries to query module

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var BPromise = require('bluebird');
 var connection = require('./db');
+var query = require('./query');
 var sampleData = require('./sample').data;
 var db;
 
@@ -7,76 +7,15 @@ connection.connect()
     .then(function (database) {
         db = database;
 
-        return clearAll('mydb');
+        return query.clearAll(db, 'mydb');
     })
     .then(function (removed) {
-        return insertInto('mydb', sampleData);
+        return query.insertInto(db, 'mydb', sampleData);
     })
     .then(function (results) {
-        return getAll('mydb');
+        return query.getAll(db, 'mydb');
     })
     .then(function (documents) {
         console.log(documents);
         connection.disconnect(db);
     });
-
-/**
- * Get all the Documents of a MongoDB Collection
- * @param   {String} collection     The MongoDB Collection name
- * @resolve {Array}  documents      The Documents of that collection
- * @reject  {Error}  err            The Error Object
- */
-var getAll = function (collection) {
-    return new BPromise(function (resolve, reject) {
-        var col = db.collection(collection);
-
-        col.find().toArray(function (err, documents) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(documents);
-            }
-        });
-    });
-};
-
-/**
- * Clear all Documents of a MongoDB Collection
- * @param   {String} collection     The MongoDB Collection name
- * @resolve {Object} removed        The Documents removed
- * @reject  {Error}  err            The Error Object
- */
-var clearAll = function (collection) {
-    return new BPromise(function (resolve, reject) {
-        var col = db.collection(collection);
-
-        col.remove({}, function (err, removed) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(removed);
-            }
-        });
-    });
-};
-
-/**
- * Inserts Documents into a MongoDB Collection
- * @param   {String} collection The MongoDB Collection name
- * @param   {Object} data       The Documents to be inserted
- * @resolve {Object} results    The results
- * @reject  {Error}  err        The Error Object
- */
-var insertInto = function (collection, data) {
-    return new BPromise(function (resolve, reject) {
-        var col = db.collection(collection);
-
-        col.insert(data, function (err, results) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(results);
-            }
-        });
-    });
-};
